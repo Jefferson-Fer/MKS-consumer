021b3854ec2f640b829ce2d4d4b2f366a1907f9f
Jefferson-Fer <jeffersonfernandesce@gmail.com>
2024-07-10 13:33:56 -0300

    lint: eslint adjustments

--- src/__tests__/components/ModalProductInCart.test.jsx
+++ src/__tests__/components/ModalProductInCart.test.jsx
@@ -1,6 +1,6 @@
 import '@testing-library/jest-dom'
 
-import { fireEvent, render } from '@testing-library/react'
+import { render } from '@testing-library/react'
 import React from 'react'
 
 import ModalProductInCart from '../../components/ModalProductInCart'
